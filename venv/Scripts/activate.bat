55621e305c6ef63016dac11a4333ddcd020dc508
vnclrd <104509587+vnclrd@users.noreply.github.com>
2025-08-29 09:17:12 +0800

    Update venv

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\ivanc\Documents\GitHub\metro-realtime\backend\venv"
+set "VIRTUAL_ENV=C:\Users\ivanc\Documents\GitHub\ulat-ph-backend\venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
